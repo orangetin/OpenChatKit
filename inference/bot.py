7bec4a6f83c1d2f21b86ef399946125d2ef262a1
orangetin <126978607+orangetin@users.noreply.github.com>
2023-05-14 17:58:56 +0000

    minor fix

--- inference/bot.py
+++ inference/bot.py
@@ -15,6 +15,28 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, Stoppi
 from accelerate import infer_auto_device_map, init_empty_weights
 import torch
 
+def sparse_model_config(model_config):
+    embedding_size = None
+    if hasattr(model_config, "hidden_size"):
+        embedding_size = model_config.hidden_size
+    elif hasattr(model_config, "n_embed"):
+        embedding_size = model_config.n_embed
+    elif hasattr(model_config, "n_embd"):
+        embedding_size = model_config.n_embd
+
+    num_head = None
+    if hasattr(model_config, "num_attention_heads"):
+        num_head = model_config.num_attention_heads
+    elif hasattr(model_config, "n_head"):
+        num_head = model_config.n_head
+
+    if embedding_size is None or num_head is None or num_head == 0:
+        raise ValueError("Check the model config")
+
+    num_embedding_size_per_head = int(embedding_size / num_head)
+    num_layer = model_config.n_layer
+
+    return num_layer, num_head, num_embedding_size_per_head
 
 def prepare_jit_inputs(inputs, model, tokenizer):
     num_batch = len(inputs)
